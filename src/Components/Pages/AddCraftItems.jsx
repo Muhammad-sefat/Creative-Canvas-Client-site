f955edb2f585244b10c67fe5a1d3017e420a8133
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-27 10:24:26 +0600

    Complete add and craft section

--- src/Components/Pages/AddCraftItems.jsx
+++ src/Components/Pages/AddCraftItems.jsx
@@ -1,7 +1,159 @@
 const AddCraftItems = () => {
+  const handleCoffeeForm = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const craft = form.craft.value;
+    const subcategory = form.subcategory.value;
+    const stock = form.stock.value;
+    const description = form.description.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const customization = form.customization.value;
+    const process = form.process.value;
+    const photo = form.photo.value;
+    const Craft = {
+      name,
+      email,
+      craft,
+      subcategory,
+      stock,
+      description,
+      price,
+      rating,
+      customization,
+      process,
+      photo,
+    };
+    console.log(Craft);
+  };
   return (
     <div>
-      <h2>This is Craft Items</h2>
+      <form
+        onSubmit={handleCoffeeForm}
+        className="border border-amber-900 rounded-md  md:w-[90%] mx-auto my-5 p-8 bg-orange-300"
+      >
+        <h1 className="text-center text-3xl font-semibold py-3">
+          Add New Craft
+        </h1>
+        <p className="md:w-[70%] mx-auto pb-5 text-center">
+          {" "}
+          Painting and drawing are timeless forms of artistic expression,
+          allowing creators to convey emotions, stories, and ideas through the
+          strokes of a brush or pencil. From intricate details in pencil
+          sketches to vibrant hues in oil paintings, each piece is a unique
+          reflection of the artist's vision and skill.
+        </p>
+        <div className="flex flex-col md:flex-row  gap-8 text-left">
+          <div className="md:w-[50%]">
+            <label className="font-semibold ">User Name</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="name"
+              placeholder="Enter user name"
+            />
+            <label className="font-semibold ">Craft Name</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="craft"
+              placeholder="Enter craft name"
+            />
+            <label className="font-semibold">Stock Status</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="stock"
+              placeholder="Stock status"
+            />
+            <br />
+            <label className="font-semibold ">Price</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="price"
+              placeholder="Enter Price"
+            />
+            <br />
+            <label className="font-semibold ">Customization</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="customization"
+              placeholder="customization"
+            />
+          </div>
+          <div className="md:w-[50%]">
+            <label className="font-semibold ">User Email</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="email"
+              placeholder="Enter user email"
+            />
+            <label className="font-semibold">Subcategory Name</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="subcategory"
+              placeholder="Enter subcategory name"
+            />
+
+            <br />
+            <label className="font-semibold ">Short Description</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="description"
+              placeholder="Enter short description"
+            />
+            <br />
+            <label className="font-semibold ">Rating</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="rating"
+              placeholder="Enter rating"
+            />
+            <br />
+            <label className="font-semibold ">Processing Time</label>
+            <br />
+            <input
+              className="border border-gray-500 p-1 rounded-md w-full mb-2"
+              type="text"
+              name="process"
+              placeholder="Processing time"
+            />
+          </div>
+        </div>
+        <div className="text-left">
+          <label className="font-semibold">Photo URL</label>
+          <br />
+          <input
+            className="border border-gray-500 p-1 rounded-md w-full mb-2"
+            type="text"
+            name="photo"
+            placeholder="Enter photo URL"
+          />
+        </div>
+        <input
+          className="w-full bg-amber-600 text-white text-xl font-semibold p-2 rounded-md my-4"
+          type="submit"
+          value=" Add Craft"
+        />
+      </form>
     </div>
   );
 };
